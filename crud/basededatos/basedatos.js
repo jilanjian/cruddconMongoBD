c19d193d1a5b519a0a7829d062d674ceebcc3a2c
jilanjian <jessica.ilanjian@gmail.com>
2017-05-30 18:31:19 -0400

    etapa 0.1

---
+++ crud/basededatos/basedatos.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database: 'crud'
+    
+});
+connection.connect();
+connection.query('select * from crud', function(err,rows, fields){
+    if (err) throw err;
+        console.log('no muestra', rows);
+    });
+
+connection.end();
+
+
