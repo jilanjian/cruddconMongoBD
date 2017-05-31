c0c294dbf74933fbb6c506d1734aa78b2a8a4b5e
jilanjian <jessica.ilanjian@gmail.com>
2017-05-31 15:21:18 -0400

    Cambios en la sintaxis de la conexion a la bd

--- crud/basededatos/basedatos.js
+++ crud/basededatos/basedatos.js
@@ -2,17 +2,24 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
-  database: 'crud'
+  password : ''
     
 });
 connection.connect();
 
-connection.query('select * from crud', function(err,rows, fields){
-    if (err) throw err;
-        console.log('no muestra', rows);
-    });
+connection.query('USE crud');
+
+query = connection.query('SELECT * FROM crud', function(err, results, fields) {
+    if (err) {
+        throw err;
+}
+  console.log(results);
+}
+);
+
 
 connection.end();
 
-module.exports = connection;
+
+
+
