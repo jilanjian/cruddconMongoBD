4b836489891635d8e1884f46b4938fcda8b12b20
jilanjian <jessica.ilanjian@gmail.com>
2017-06-09 09:26:16 -0400

    Prueba y Conexion de aplicación con MongolBD / Estado: Positiva

---
+++ crudMongolDB/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var mongoose = require ('mongoose');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('<h1>SI CONECTO AL PUERTO 27017</h1>');
+    
+});
+
+app.listen(27017);
+console.log('Servidor Express escuchando en puerto 27017');
