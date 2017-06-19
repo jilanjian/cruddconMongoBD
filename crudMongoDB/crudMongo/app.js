d7521bc8b12c584999828797b52d7af32d6afa0d
jilanjian <jessica.ilanjian@gmail.com>
2017-06-19 15:34:20 -0400

    Inserción del Body a NJs

--- crudMongoDB/crudMongo/app.js
+++ crudMongoDB/crudMongo/app.js
@@ -2,14 +2,19 @@
 
 const express = require('express')
 const bodyParser = require ('body-parser')
+const hbs = require('express-handlebars')
 const app = express()
 const api = require('./routes')
+
 
     
 app.use(bodyParser.urlencoded({ extended : false }))
 app.use(bodyParser.json())
 app.use('/api', api)
-
+app.set('view engine', 'jade')
+app.get('/', function (req, res) {
+  res.render('index')
+})
 
 
 
